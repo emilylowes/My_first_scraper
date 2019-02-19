582054fbfb557eb83d9a048f7e29e00e4b9b942c
emilylowes <44525402+emilylowes@users.noreply.github.com>
2019-02-19 11:23:09 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,6 +8,7 @@ print("hello")
 # # Read in a page
 html = scraperwiki.scrape("http://foo.com")
 #
+print (html)
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
